f049b9bfbaf8006f8e4a1c012c1937193ae65512
Jessedev1 <jesse@creativework.nl>
2023-07-03 16:30:48 +0200

    DrawableHistory

---
+++ DesignPatterns/DrawableHistory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class DrawableHistory
+    {
+        Stack<Drawable> history = new Stack<Drawable>();
+        public void push(Drawable drawable)
+        {
+            history.Push(drawable);
+        }
+
+        public void pop()
+        {
+            history.Pop();
+        }
+    }
+}
