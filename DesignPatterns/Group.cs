16540d80dc579ad02b97d9ed6c6abe256d8f65fa
Ruben-Engelage <ruben.engelage@student.nhlstenden.com>
2023-07-03 18:38:54 +0200

    Composite Base - Group

---
+++ DesignPatterns/Group.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns
+{
+    public class Group<T>
+    {
+        private List<Group<T>> Children = new List<Group<T>>();
+
+        public List<Group<T>> GroupObject(Group<T> group)
+        {
+            Children.Add(group);
+            return Children;
+        }
+        
+        public List<Group<T>> GroupObjects(List<Group<T>> groups)
+        {
+            Children.AddRange(groups);
+            return Children;
+        }
+
+        public List<Group<T>> GetChildren()
+        {
+            return Children;
+        }
+        
+        public List<Group<T>> RemoveObject(Group<T> group)
+        {
+            Children.Remove(group);
+            return Children;
+        }
+        
+        public List<Group<T>> RemoveObjects(List<Group<T>> groups)
+        {
+            Children.RemoveAll(groups.Contains);
+            return Children;
+        }
+    }
+}
