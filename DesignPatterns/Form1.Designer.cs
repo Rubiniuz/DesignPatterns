0033a40728f49ebddd712839d5f63c30d97ab05c
Ruben-Engelage <ruben.engelage@student.nhlstenden.com>
2023-07-03 15:26:21 +0200

    laad image

--- DesignPatterns/Form1.Designer.cs
+++ DesignPatterns/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace DesignPatterns
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.TopPanel = new System.Windows.Forms.Panel();
+            this.load_button = new System.Windows.Forms.Button();
             this.exit_button = new System.Windows.Forms.Button();
             this.clear_button = new System.Windows.Forms.Button();
             this.save_button = new System.Windows.Forms.Button();
@@ -38,27 +39,28 @@ namespace DesignPatterns
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.canvasPanel = new System.Windows.Forms.Panel();
             this.toolboxPanel = new System.Windows.Forms.Panel();
+            this.ellipse_button = new System.Windows.Forms.PictureBox();
             this.rectangle_button = new System.Windows.Forms.PictureBox();
             this.colorbox = new System.Windows.Forms.PictureBox();
             this.paintbrush_size = new System.Windows.Forms.NumericUpDown();
             this.eraser_button = new System.Windows.Forms.PictureBox();
             this.paintbrush_button = new System.Windows.Forms.PictureBox();
-            this.ellipse_button = new System.Windows.Forms.PictureBox();
             this.TopPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.canvasPanel.SuspendLayout();
             this.toolboxPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ellipse_button)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.rectangle_button)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.colorbox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.paintbrush_size)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.eraser_button)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.paintbrush_button)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ellipse_button)).BeginInit();
             this.SuspendLayout();
             // 
             // TopPanel
             // 
             this.TopPanel.BackColor = System.Drawing.SystemColors.Highlight;
+            this.TopPanel.Controls.Add(this.load_button);
             this.TopPanel.Controls.Add(this.exit_button);
             this.TopPanel.Controls.Add(this.clear_button);
             this.TopPanel.Controls.Add(this.save_button);
@@ -66,18 +68,31 @@ namespace DesignPatterns
             this.TopPanel.Controls.Add(this.pictureBox1);
             this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Margin = new System.Windows.Forms.Padding(4);
             this.TopPanel.Name = "TopPanel";
-            this.TopPanel.Size = new System.Drawing.Size(800, 51);
+            this.TopPanel.Size = new System.Drawing.Size(1067, 63);
             this.TopPanel.TabIndex = 0;
             this.TopPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseDown);
             this.TopPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseMove);
             this.TopPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseUp);
             // 
+            // load_button
+            // 
+            this.load_button.Location = new System.Drawing.Point(577, 17);
+            this.load_button.Margin = new System.Windows.Forms.Padding(4);
+            this.load_button.Name = "load_button";
+            this.load_button.Size = new System.Drawing.Size(100, 28);
+            this.load_button.TabIndex = 4;
+            this.load_button.Text = "Laden";
+            this.load_button.UseVisualStyleBackColor = true;
+            this.load_button.Click += new System.EventHandler(this.load_button_Click);
+            // 
             // exit_button
             // 
-            this.exit_button.Location = new System.Drawing.Point(713, 14);
+            this.exit_button.Location = new System.Drawing.Point(951, 17);
+            this.exit_button.Margin = new System.Windows.Forms.Padding(4);
             this.exit_button.Name = "exit_button";
-            this.exit_button.Size = new System.Drawing.Size(75, 23);
+            this.exit_button.Size = new System.Drawing.Size(100, 28);
             this.exit_button.TabIndex = 3;
             this.exit_button.Text = "Sluiten";
             this.exit_button.UseVisualStyleBackColor = true;
@@ -85,9 +100,10 @@ namespace DesignPatterns
             // 
             // clear_button
             // 
-            this.clear_button.Location = new System.Drawing.Point(595, 14);
+            this.clear_button.Location = new System.Drawing.Point(793, 17);
+            this.clear_button.Margin = new System.Windows.Forms.Padding(4);
             this.clear_button.Name = "clear_button";
-            this.clear_button.Size = new System.Drawing.Size(75, 23);
+            this.clear_button.Size = new System.Drawing.Size(100, 28);
             this.clear_button.TabIndex = 2;
             this.clear_button.Text = "Legen";
             this.clear_button.UseVisualStyleBackColor = true;
@@ -95,9 +111,10 @@ namespace DesignPatterns
             // 
             // save_button
             // 
-            this.save_button.Location = new System.Drawing.Point(514, 14);
+            this.save_button.Location = new System.Drawing.Point(685, 17);
+            this.save_button.Margin = new System.Windows.Forms.Padding(4);
             this.save_button.Name = "save_button";
-            this.save_button.Size = new System.Drawing.Size(75, 23);
+            this.save_button.Size = new System.Drawing.Size(100, 28);
             this.save_button.TabIndex = 1;
             this.save_button.Text = "Opslaan";
             this.save_button.UseVisualStyleBackColor = true;
@@ -107,18 +124,20 @@ namespace DesignPatterns
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(64, 19);
+            this.label1.Location = new System.Drawing.Point(85, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.Size = new System.Drawing.Size(112, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Teken applicatie";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-2, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(60, 54);
+            this.pictureBox1.Size = new System.Drawing.Size(80, 66);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -128,9 +147,10 @@ namespace DesignPatterns
             this.canvasPanel.BackColor = System.Drawing.Color.White;
             this.canvasPanel.Controls.Add(this.toolboxPanel);
             this.canvasPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.canvasPanel.Location = new System.Drawing.Point(0, 51);
+            this.canvasPanel.Location = new System.Drawing.Point(0, 63);
+            this.canvasPanel.Margin = new System.Windows.Forms.Padding(4);
             this.canvasPanel.Name = "canvasPanel";
-            this.canvasPanel.Size = new System.Drawing.Size(800, 399);
+            this.canvasPanel.Size = new System.Drawing.Size(1067, 491);
             this.canvasPanel.TabIndex = 1;
             this.canvasPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
             this.canvasPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
@@ -147,16 +167,28 @@ namespace DesignPatterns
             this.toolboxPanel.Controls.Add(this.paintbrush_button);
             this.toolboxPanel.Dock = System.Windows.Forms.DockStyle.Left;
             this.toolboxPanel.Location = new System.Drawing.Point(0, 0);
+            this.toolboxPanel.Margin = new System.Windows.Forms.Padding(4);
             this.toolboxPanel.Name = "toolboxPanel";
-            this.toolboxPanel.Size = new System.Drawing.Size(58, 399);
+            this.toolboxPanel.Size = new System.Drawing.Size(77, 491);
             this.toolboxPanel.TabIndex = 0;
+            // 
+            // ellipse_button
+            // 
+            this.ellipse_button.Image = ((System.Drawing.Image)(resources.GetObject("ellipse_button.Image")));
+            this.ellipse_button.Location = new System.Drawing.Point(9, 197);
+            this.ellipse_button.Name = "ellipse_button";
+            this.ellipse_button.Size = new System.Drawing.Size(57, 57);
+            this.ellipse_button.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ellipse_button.TabIndex = 5;
+            this.ellipse_button.TabStop = false;
+            this.ellipse_button.Click += new System.EventHandler(this.ellipse_button_Click);
             // 
             // rectangle_button
             // 
             this.rectangle_button.Image = ((System.Drawing.Image)(resources.GetObject("rectangle_button.Image")));
-            this.rectangle_button.Location = new System.Drawing.Point(7, 109);
+            this.rectangle_button.Location = new System.Drawing.Point(9, 134);
             this.rectangle_button.Name = "rectangle_button";
-            this.rectangle_button.Size = new System.Drawing.Size(43, 41);
+            this.rectangle_button.Size = new System.Drawing.Size(57, 57);
             this.rectangle_button.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.rectangle_button.TabIndex = 4;
             this.rectangle_button.TabStop = false;
@@ -165,32 +197,31 @@ namespace DesignPatterns
             // colorbox
             // 
             this.colorbox.BackColor = System.Drawing.Color.Black;
-            this.colorbox.Location = new System.Drawing.Point(0, 346);
+            this.colorbox.Location = new System.Drawing.Point(0, 426);
+            this.colorbox.Margin = new System.Windows.Forms.Padding(4);
             this.colorbox.Name = "colorbox";
-            this.colorbox.Size = new System.Drawing.Size(58, 53);
+            this.colorbox.Size = new System.Drawing.Size(77, 65);
             this.colorbox.TabIndex = 3;
             this.colorbox.TabStop = false;
             this.colorbox.Click += new System.EventHandler(this.colorbox_Click);
             // 
             // paintbrush_size
             // 
-            this.paintbrush_size.Location = new System.Drawing.Point(0, 325);
+            this.paintbrush_size.Location = new System.Drawing.Point(0, 400);
+            this.paintbrush_size.Margin = new System.Windows.Forms.Padding(4);
             this.paintbrush_size.Name = "paintbrush_size";
-            this.paintbrush_size.Size = new System.Drawing.Size(58, 20);
+            this.paintbrush_size.Size = new System.Drawing.Size(77, 22);
             this.paintbrush_size.TabIndex = 2;
-            this.paintbrush_size.Value = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
+            this.paintbrush_size.Value = new decimal(new int[] { 1, 0, 0, 0 });
             this.paintbrush_size.ValueChanged += new System.EventHandler(this.paintbrushsize_change);
             // 
             // eraser_button
             // 
             this.eraser_button.Image = ((System.Drawing.Image)(resources.GetObject("eraser_button.Image")));
-            this.eraser_button.Location = new System.Drawing.Point(7, 61);
+            this.eraser_button.Location = new System.Drawing.Point(9, 75);
+            this.eraser_button.Margin = new System.Windows.Forms.Padding(4);
             this.eraser_button.Name = "eraser_button";
-            this.eraser_button.Size = new System.Drawing.Size(43, 42);
+            this.eraser_button.Size = new System.Drawing.Size(57, 52);
             this.eraser_button.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.eraser_button.TabIndex = 1;
             this.eraser_button.TabStop = false;
@@ -199,33 +230,24 @@ namespace DesignPatterns
             // paintbrush_button
             // 
             this.paintbrush_button.Image = ((System.Drawing.Image)(resources.GetObject("paintbrush_button.Image")));
-            this.paintbrush_button.Location = new System.Drawing.Point(7, 9);
+            this.paintbrush_button.Location = new System.Drawing.Point(9, 11);
+            this.paintbrush_button.Margin = new System.Windows.Forms.Padding(4);
             this.paintbrush_button.Name = "paintbrush_button";
-            this.paintbrush_button.Size = new System.Drawing.Size(43, 46);
+            this.paintbrush_button.Size = new System.Drawing.Size(57, 57);
             this.paintbrush_button.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.paintbrush_button.TabIndex = 0;
             this.paintbrush_button.TabStop = false;
             this.paintbrush_button.Click += new System.EventHandler(this.paintbrush_button_Click);
             // 
-            // ellipse_button
-            // 
-            this.ellipse_button.Image = ((System.Drawing.Image)(resources.GetObject("ellipse_button.Image")));
-            this.ellipse_button.Location = new System.Drawing.Point(7, 157);
-            this.ellipse_button.Name = "ellipse_button";
-            this.ellipse_button.Size = new System.Drawing.Size(43, 40);
-            this.ellipse_button.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.ellipse_button.TabIndex = 5;
-            this.ellipse_button.TabStop = false;
-            this.ellipse_button.Click += new System.EventHandler(this.ellipse_button_Click);
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.canvasPanel);
             this.Controls.Add(this.TopPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.TopPanel.ResumeLayout(false);
@@ -233,15 +255,16 @@ namespace DesignPatterns
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.canvasPanel.ResumeLayout(false);
             this.toolboxPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ellipse_button)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.rectangle_button)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.colorbox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.paintbrush_size)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.eraser_button)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.paintbrush_button)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ellipse_button)).EndInit();
             this.ResumeLayout(false);
-
         }
+
+        private System.Windows.Forms.Button load_button;
 
         #endregion
 
