8b68217b9cede6607b0f18e0acc146341650cc94
Ruben-Engelage <ruben.engelage@student.nhlstenden.com>
2023-07-03 19:00:37 +0200

    Add Visitor

---
+++ DesignPatterns/SizeVisitor.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPatterns
+{
+    public class SizeVisitor : Visitor
+    {
+        public void VisitRectangle(RectangleDrawable rect)
+        {
+            //TODO Resize Rectangle
+        }
+        public void VisitEllipse(EllipseDrawable elip)
+        {
+            //TODO Resize Ellipse
+        }
+    }
+}
