faa4791f18624f151d705cb55ce6726ef9934b30
Ruben-Engelage <ruben.engelage@student.nhlstenden.com>
2023-07-03 18:04:17 +0200

    Add collision Check for rectangle and Ellipse

--- DesignPatterns/Drawables.cs
+++ DesignPatterns/Drawables.cs
@@ -17,6 +17,7 @@ namespace DesignPatterns
         public Drawable() { }
 
         public abstract void draw(Graphics graph);
+        public abstract bool isInBounds(Point p);
     }
 
     public class LineDrawable : Drawable
@@ -31,6 +32,11 @@ namespace DesignPatterns
         public override void draw(Graphics graph)
         {
             graph.DrawLine(pen, startPos, endPos);
+        }
+
+        public override bool isInBounds(Point p)
+        {
+            throw new NotImplementedException();
         }
     }
 
@@ -51,6 +57,43 @@ namespace DesignPatterns
             Rectangle rect = new Rectangle(startPos.X, startPos.Y, width * Math.Sign(width), height * Math.Sign(height));
             graph.DrawEllipse(pen, rect);
         }
+        
+        public override bool isInBounds(Point p)
+        {
+            Point MinVal = new Point();
+            Point Maxval = new Point();
+
+            if (startPos.X < endPos.X)
+            {
+                MinVal.X = startPos.X;
+                Maxval.X = endPos.X;
+            }
+            else
+            {
+                MinVal.X = endPos.X;
+                Maxval.X = startPos.X;
+            }
+            
+            if (startPos.Y < endPos.Y)
+            {
+                MinVal.Y = startPos.Y;
+                Maxval.Y = endPos.Y;
+            }
+            else
+            {
+                MinVal.Y = endPos.Y;
+                Maxval.Y = startPos.Y;
+            }
+
+            if (p.X > MinVal.X && p.X < Maxval.X && p.Y > MinVal.Y && p.Y < Maxval.Y)
+            {
+                return true; // Within Bounds
+            }
+            else
+            {
+                return false; // Not in Bounds
+            }
+        }
     }
 
     public class EllipseDrawable : Drawable
@@ -70,6 +113,20 @@ namespace DesignPatterns
 
             Rectangle rect = new Rectangle(startPos.X, startPos.Y, width * Math.Sign(width), height * Math.Sign(height));
             graph.DrawEllipse(pen, rect);
+        }
+        
+        public override bool isInBounds(Point p)
+        {
+            int width = endPos.X - startPos.X;
+            int height = endPos.Y - startPos.Y;
+            
+            
+            double res = ((double)Math.Pow((p.X - startPos.X), 2)
+                    / (double)Math.Pow(width, 2))
+                   + ((double)Math.Pow((p.Y - startPos.Y), 2)
+                      / (double)Math.Pow(height, 2));
+                      
+            return res <= 1 ? true : false;
         }
     }
 }
